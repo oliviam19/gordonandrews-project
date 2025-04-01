a94739fcefdc582c1f5c8cbddd3f587c8e16781c
oliviam19 <OliviaMilner1912@gmail.com>
2025-04-01 12:44:01 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 window.addEventListener("beforeunload", function () {
     window.scrollTo(0, 0);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery__item img');
+
+    images.forEach(img => {
+        const originalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('click', function() {
+            if (img.src === originalSrc) {
+                img.src = hoverSrc;
+            } else {
+                img.src = originalSrc;
+            }
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery__item img');
+
+    images.forEach(img => {
+        const originalSrc = img.src;
+        const hoverSrc = img.getAttribute('data-hover');
+
+        img.addEventListener('mouseenter', function() {
+            img.src = hoverSrc;
+        });
+
+        img.addEventListener('mouseleave', function() {
+            img.src = originalSrc;
+        });
+    });
+});
+
+
+
